e6f138ab69ebcef9b6cbacbb248a817b3c530ebd
imckh kompakar <987992518@qq.com>
2019-05-09 19:08:18 +0800

    junit测试

--- git-blog-api/src/test/java/com/imckh/gitblog/api/HelloControllerTest.java
+++ git-blog-api/src/test/java/com/imckh/gitblog/api/HelloControllerTest.java
@@ -5,9 +5,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -19,10 +21,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
+@SpringBootTest
 @ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
 public class HelloControllerTest {
 
-    @Autowired
     private MockMvc mockMvc;
 
     /**
