56d59829de88da40f4e7d153579951c537171f40
imckh <987992518@qq.com>
2019-06-25 23:18:09 +0800

    更新数据库

--- gitblog.sql
+++ gitblog.sql
@@ -1,67 +1,97 @@
-create schema gitblog collate utf8mb4_0900_ai_ci;
+-- MySQL dump 10.13  Distrib 8.0.16, for Linux (x86_64)
+--
+-- Host: localhost    Database: gitblog
+-- ------------------------------------------------------
+-- Server version       8.0.16
 
-create table role
-(
-	id int(11) unsigned auto_increment
-		primary key,
-	role varchar(50) not null comment '角色名',
-	permission varchar(50) null comment '权限',
-	constraint role_role_uindex
-		unique (role)
-)
-comment '角色表';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8mb4 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-create table user
-(
-	id int(11) unsigned auto_increment
-		primary key,
-	username varchar(50) not null comment '用户名',
-	password varchar(100) not null comment '密码',
-	role_id int not null comment '关联角色表',
-	ban varchar(3) default '0' not null comment '状态: 0正常, 1禁用',
-	create_datetime datetime not null,
-	constraint user_username_uindex
-		unique (username)
-)
-comment '用户表';
+--
+-- Table structure for table `hibernate_sequence`
+--
 
+DROP TABLE IF EXISTS `hibernate_sequence`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `hibernate_sequence` (
+  `next_val` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-create table role
-(
-    id         int(11) unsigned auto_increment
-        primary key,
-    role       varchar(50) not null comment '角色名',
-    permission varchar(50) null comment '权限',
-    constraint role_role_uindex
-        unique (role)
-)
-    comment '角色表';
+--
+-- Table structure for table `profile`
+--
 
+DROP TABLE IF EXISTS `profile`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `profile` (
+  `profile_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '用户简介表id',
+  `user_id` int(10) unsigned DEFAULT NULL,
+  `bio` varchar(500) DEFAULT NULL COMMENT '个人简介',
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`profile_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='用户简介表';
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-create table user
-(
-	id int(11) unsigned auto_increment
-		primary key,
-	username varchar(50) not null comment '用户名',
-	password varchar(100) not null comment '密码',
-	role_id int not null comment '关联角色表',
-	ban varchar(3) default '0' not null comment '状态: 0正常, 1禁用',
-	create_datetime datetime not null,
-	constraint user_username_uindex
-		unique (username)
-)
-comment '用户表';
+--
+-- Table structure for table `role`
+--
 
+DROP TABLE IF EXISTS `role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `role` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `role` varchar(50) NOT NULL COMMENT '角色名',
+  `permission` varchar(50) DEFAULT NULL COMMENT '权限',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `role_role_uindex` (`role`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='角色表';
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `user`
+--
 
-INSERT INTO gitblog.role (id, role, permission) VALUES (1, 'user', 'normal');
-INSERT INTO gitblog.role (id, role, permission) VALUES (2, 'admin', 'vip');
-INSERT INTO gitblog.user (id, username, password, role_id, ban, create_datetime) VALUES (4, 'ckh', '0000', 2, '0', '2019-05-16 14:39:22');
-INSERT INTO gitblog.user (id, username, password, role_id, ban, create_datetime) VALUES (5, 'ckh2', '0000', 1, '0', '2019-05-16 14:39:42');
-<<<<<<< Updated upstream
-INSERT INTO gitblog.user (id, username, password, role_id, ban, create_datetime) VALUES (6, 'ckh3', '0000', 1, '0', '2017-05-16 14:40:02');
-=======
-INSERT INTO gitblog.user (id, username, password, role_id, ban, create_datetime) VALUES (6, 'ckh3', '0000', 1, '0', '2017-05-16 14:40:02');
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `user` (
+  `user_id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '用户表id',
+  `login` varchar(50) NOT NULL COMMENT '登录用户名 可以作为登陆id',
+  `username` varchar(50) NOT NULL COMMENT '用户名 不可作为登录id',
+  `password` varchar(100) NOT NULL COMMENT '密码',
+  `email` varchar(100) DEFAULT NULL COMMENT '邮箱 可以作为登陆id',
+  `role_id` int(11) NOT NULL COMMENT '关联角色表',
+  `ban` varchar(3) NOT NULL DEFAULT '0' COMMENT '状态: 0正常, 1禁用',
+  `created_at` datetime NOT NULL COMMENT '创建时间',
+  `updated_at` datetime NOT NULL COMMENT '更新时间',
+  `token` varchar(100) NOT NULL COMMENT 'JSON Web Token（JWT）',
+  `token_expiry_at` datetime DEFAULT NULL COMMENT 'token到期时间 null表示永久',
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `user_username_uindex` (`username`),
+  UNIQUE KEY `user_login_uindex` (`login`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='用户表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-COMMIT;
->>>>>>> Stashed changes
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-06-25 15:13:34
